8eeb1a8da1f9405ee13f47cdadc075d4172418a3
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2022-07-26 21:03:46 +0700

    Implemented dummy Vue.js component bundled with Bili

---
+++ IDocumentViewer.vue
@@ -0,0 +1,15 @@
+<template>
+  <i-button :loading="true">
+      Document Viewer
+      <template #loading>
+          <i-loader color="dark" class="_margin-right:1/2" />
+          Loading
+      </template>
+  </i-button>
+</template>
+
+<script>
+export default {
+  name: "IDocumentViewer"
+}
+</script>
--- index.js
+++
@@ -1,3 +0,0 @@
-export default function viewer() {
-    console.log("Hello world!")
-}
